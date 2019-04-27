6f5be4b8274bb1715302b1b7a3e91ffd35d0fc9e
Henk Tillman <henktillman@gmail.com>
2019-04-26 22:39:09 -0700

    u

--- train.py
+++ train.py
@@ -139,10 +139,10 @@ class NeuralNet(nn.Module):
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.BatchNorm1D(50),
+            nn.BatchNorm1d(50),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
-            nn.BatchNorm1D(50),
+            nn.BatchNorm1d(50),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
